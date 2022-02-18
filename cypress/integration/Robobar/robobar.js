6787cc8d8d1f0550ef33631e74cb5df40a9dec36
joancorderroure-gft <JNOM@gft.com>
2022-02-18 13:16:08 +0100

    modified tests

--- cypress/integration/Robobar/robobar.js
+++ cypress/integration/Robobar/robobar.js
@@ -1,41 +1,41 @@
 import { Given } from "cypress-cucumber-preprocessor/steps";
-import { Then} from "cypress-cucumber-preprocessor/steps" 
-import { When } from "cypress-cucumber-preprocessor/steps"
+import { Then} from "cypress-cucumber-preprocessor/steps";
+import { When } from "cypress-cucumber-preprocessor/steps";
 
 //Functions
- colaButton() {
+    function colaButton() {
         return cy.get(':nth-child(1) > :nth-child(3) > .row > .col-5 > .input-group-append > .btn');
-    },
+    }
 
-    beerButton() {
+   function beerButton() {
         return cy.get(':nth-child(2) > :nth-child(3) > .row > .col-5 > .input-group-append > .btn');
-    },
+    }
 
-    wineButton() {
+   function wineButton() {
         return cy.get(':nth-child(3) > :nth-child(3) > .row > .col-5 > .input-group-append > .btn');
-    },
+    }
 
-    totalText() {
+   function totalText() {
         return cy.get(':nth-child(4) > .ng-binding');
-    },
+    }
 
-    checkOut() {
+   function checkOut() {
         return cy.get('.col-12 > .btn');
-    },
+    }
 
-    ageInput() {
+   function ageInput() {
         return cy.get('#ageInput');
-    },
+    }
 
-    orderButton() {
+   function orderButton() {
         return cy.get('.btn-success');
-    },
+    }
 
-    confirmText() {
+   function confirmText() {
             return cy.get('p');
-    },
+    }
 
-    alertAge() {
+   function alertAge() {
         return cy.get(':nth-child(2) > :nth-child(1) > .alert');
     }
 
@@ -47,27 +47,27 @@ Given('user opens robobar website', () => {
 })
 
 When('user adds a cola', () => {
-    robobar.colaButton().click();
+    colaButton().click();
 })
 
 
 Then('total should be {string}', (total) => {
-    robobar.totalText().should("contain.text", total);
+    totalText().should("contain.text", total);
 })
 
 When('user adds a beer', () => {
-    robobar.beerButton().click();
+    beerButton().click();
 })
 
 When('user adds {int} cola', (n) => {
     for(let i=0; i<n; i++) {
-        robobar.colaButton().click();
+        colaButton().click();
     }
 })
 
 
 Then('total should be €{float}', (total) => {
-    robobar.totalText().should("contain", "€"+total);
+    totalText().should("contain", "€"+total);
 })
 
 
